03b14a6f14e4937a18ddd88e2c20978c607129e6
Michael B <webmikez@gmail.com>
2014-05-19 14:49:46 +0700

    Почти рабочая версия табов на внутренней странице

--- js/inner.js
+++ js/inner.js
@@ -10,7 +10,6 @@ $(document).ready(function() {
     $('.section-sub-nav a').bind('click', function (e) {
         var hash = this.hash;
         e.preventDefault()
-        $('div'+hash).addClass('fade active in');
         $(this).tab('show');
         $(this).closest('.tab-pane').children('.tab-pane-content-holder').hide()
     });
